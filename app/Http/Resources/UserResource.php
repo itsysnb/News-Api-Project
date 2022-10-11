04a20557b1c7d71a4a9e623c2bcdaa11fcd4177e
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-12 02:15:14 +0330

    add field to UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,10 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'fullName' => $this->first_name . ' ' . $this->last_name,
+            'email' => $this->email,
+        ];
     }
 }
