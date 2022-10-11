13db9babd493022884640333f56c435441bcdb69
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-12 01:59:49 +0330

    Added ApiController

--- app/Http/Controllers/Api/v1/Auth/SanctumController.php
+++ app/Http/Controllers/Api/v1/Auth/SanctumController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Api\v1\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class SanctumController extends Controller
 {
     public function register(RegisterRequest $request)
     {
-
+        $user = User::create($request->validated());
     }
 }
