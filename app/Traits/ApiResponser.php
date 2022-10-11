0110a06c95c2fcbb91ae8f0d49d6ff9881b4afe5
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-12 02:07:57 +0330

    Add functions to ApiResponser

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+trait ApiResponser
+{
+    public function successResponse($data, int $code = Response::HTTP_OK): JsonResponse
+    {
+        return response()->json(['data' => $data, 'code' => $code], $code, [], JSON_UNESCAPED_UNICODE);
+    }
+
+    public function errorResponse($data, int $code): JsonResponse
+    {
+        return response()->json(['error' => $data, 'code' => $code], $code, [], JSON_UNESCAPED_UNICODE);
+    }
+}
