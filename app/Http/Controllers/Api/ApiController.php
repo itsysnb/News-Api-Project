d25b8eeca5f5666655ba15a67d6cac94f6bc5990
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-12 02:10:19 +0330

    Complete ApiController

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    use ApiResponser;
 }
