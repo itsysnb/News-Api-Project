b41c5b6790fd3bae65b31df2b67ec2036c2c07b8
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-14 00:35:28 +0330

    Complete CategoryResource

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -14,6 +14,9 @@ class CategoryResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'category_name' => $this->name,
+            'category_detail' => $this->description,
+        ];
     }
 }
