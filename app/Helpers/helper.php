d66afa48a159ef97be8e3198e54dd54aa4287fb0
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-13 19:57:28 +0330

    Define checkPassword function to helper

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,1 +1,11 @@
 <?php
+
+use Illuminate\Validation\ValidationException;
+
+function checkPassword($requestPwd, $originalPwd)
+{
+    if (!$requestPwd == $originalPwd) {
+        throw ValidationException::withMessages(['password' => 'The provided credentials are incorrect.']);
+    }
+
+}
