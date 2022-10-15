0612bb08650f7db40570dc5933e24deaf3628e13
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-15 22:58:04 +0330

    Complete PostResource

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -14,6 +14,11 @@ class PostResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'title' => $this->title,
+            'description' => $this->description,
+            'author' => $this->user,
+            'category' => $this->category
+        ];
     }
 }
