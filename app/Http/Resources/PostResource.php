ee91c206e6f028186d91014ef598b15499b29aee
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-16 00:52:11 +0330

    add id column to post

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -15,6 +15,7 @@ class PostResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'author' => $this->user,
