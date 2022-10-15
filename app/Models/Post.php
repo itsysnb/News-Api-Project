bedf9406148c9da91cee081baf199be01e015d5e
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-15 22:54:48 +0330

    Define Relations In Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -15,4 +16,14 @@ class Post extends Model
         'user_id',
         'category_id',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
