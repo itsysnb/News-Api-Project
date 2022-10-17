4c76fc2f4afe2d2e43ac1dce6de3b8d919f85bc6
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-18 00:44:04 +0330

    complete commentResource

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -14,6 +14,11 @@ class CommentResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'subject' => $this->subject,
+            'body' => $this->body,
+            'user_id' => UserResource::collection($this->whenLoaded('user')),
+            'post_id' => PostResource::collection($this->whenLoaded('post')),
+        ];
     }
 }
