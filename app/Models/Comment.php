7900f82f3b54bd06dee4536f7a85f9b637581d11
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-18 00:07:39 +0330

    Add new Functions to Comment Model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
@@ -15,4 +16,14 @@ class Comment extends Model
         'user_id',
         'post_id',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
