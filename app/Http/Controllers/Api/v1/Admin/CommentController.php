cfbd21b311cc2cf7e9aa6d418fb88337ee089e21
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-18 00:26:15 +0330

    Complete CommentController

--- app/Http/Controllers/Api/v1/Admin/CommentController.php
+++ app/Http/Controllers/Api/v1/Admin/CommentController.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Controllers\Api\v1\Admin;
 
+use App\Http\Controllers\Api\ApiController;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CommentRequest;
+use App\Http\Resources\CommentResource;
+use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
-class CommentController extends Controller
+class CommentController extends ApiController
 {
     public function store(CommentRequest $request)
     {
+        $attributes = Comment::create($request->validated());
+        return $this->successResponse(CommentResource::make($attributes), Response::HTTP_CREATED);
+    }
 
+    public function show(Post $post)
+    {
+        $comment = Comment::where('post_id', $post->id)->get();
+        return $this->successResponse(CommentResource::collection($comment));
     }
 }
