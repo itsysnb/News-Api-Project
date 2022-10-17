de9f81ee129d920f3e268d51958adebedac7d7cb
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-17 23:57:53 +0330

    Define Comment

--- routes/api.php
+++ routes/api.php
@@ -51,5 +51,7 @@ Route::group([
         Route::get('posts/{post}', [\App\Http\Controllers\Api\v1\Admin\PostController::class , 'show'])->name('posts.show');
         Route::patch('posts/{post}', [\App\Http\Controllers\Api\v1\Admin\PostController::class, 'update'])->name('posts.update');
         Route::delete('posts/{post}', [\App\Http\Controllers\Api\v1\Admin\PostController::class, 'destroy'])->name('posts.destroy');
+        // comment routes
+
     });
 });
